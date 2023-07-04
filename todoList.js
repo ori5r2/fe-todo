550298bbea43e90e661c6d62f8c016418cb62480
ori5r2 <ori5ri512@gmail.com>
2023-07-04 11:58:16 +0900

    add "show" feature

--- todoList.js
+++ todoList.js
@@ -18,6 +18,36 @@ class TodoList {
       },
     ];
   }
+
+  // TodoList 보여주는 함수
+  show(status) {
+    if (status == "all") {
+      // 전체 todo의 상태 보여주기
+      const cnt = { todo: 0, doing: 0, done: 0 };
+      this.todo_list.map((todo) => {
+        if (todo.status === "todo") cnt.todo += 1;
+        else if (todo.status === "doing") cnt.doing += 1;
+        else if (todo.status === "done") cnt.done += 1;
+      });
+
+      console.log(
+        `현재상태: todo: ${cnt.todo}, doing: ${cnt.doing}, done: ${cnt.done}`
+      );
+    } else {
+      // status에 해당하는 todo 보여주기
+      let arr = [];
+      this.todo_list.map((todo) => {
+        if (todo.status === status) {
+          arr.push({ name: todo.name, id: todo.id });
+        }
+      });
+
+      console.log(`${status}리스트 :  총${arr.length}건 : `);
+      arr.forEach((x) => {
+        console.log(`${x.name}, ${x.id}`);
+      });
+    }
+  }
 }
 
 module.exports = TodoList;
